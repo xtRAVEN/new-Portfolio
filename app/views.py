af8c12887261f43d2b67caca35050de4b51df0f5
xtLUCIFER <umr0042@gmail.com>
2023-06-21 09:53:54 -0400

    caching policy appllied

--- app/views.py
+++ app/views.py
@@ -1,17 +1,51 @@
 from django.shortcuts import render
 from .models import projectsphoto,projectscount,contact
 from django.http import HttpResponse
-# Create your views here.
+from django.core.cache import cache
+from django.shortcuts import render
+from django.dispatch import receiver
+from django.core.cache import cache
+from django.db.models.signals import post_save
+
+@receiver(post_save, sender=(projectsphoto))
+def contact_saved(sender, instance, created, **kwargs):
+    if created:
+        cache.delete('index_data') 
+@receiver(post_save, sender=(projectscount))
+def contact_saved(sender, instance, created, **kwargs):
+    if created:
+        cache.delete('index_data') 
 
 
+        
 def index(request):
+    # Try to get the cached data
+    cached_data = cache.get('index_data')
+
+    if cached_data is None:
+        # Data is not in the cache, fetch it from the database
+        print("data cached")
+        nav = ['Home', 'Skills', 'Projects', 'Contact']
+        photo = projectsphoto.objects.all()
+        count = projectscount.objects.all()
+        
+        # Store the data in the cache for future requests
+        cache.set('index_data', {'nav': nav, 'photo': photo, 'count': count})
+        
+    else:
+        print("already cached data used ")
+        # Use the cached data
+        nav = cached_data['nav']
+        photo = cached_data['photo']
+        count = cached_data['count']
+        
+       
     if request.method == 'POST':
+        # Handle the POST request
         email = request.POST.get('email')
         name = request.POST.get('name')
         message = request.POST.get('message')  
-        contact.objects.create(email=email,name=name,messafe=message)
-        return HttpResponse("your message recieved we'll contact you soon.")
-    nav = ['Home','Skills','Projects','Contact']
-    photo = projectsphoto.objects.all()
-    count = projectscount.objects.all()
-    return render(request,'index.html',{'nav':nav,'photo':photo,'count':count})
+        contact.objects.create(email=email, name=name, message=message)
+        return HttpResponse("Your message received. We'll contact you soon.")
+
+    return render(request, 'index.html', {'nav': nav, 'photo': photo, 'count': count})
--- myweb/settings.py
+++ myweb/settings.py
@@ -130,3 +130,13 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# settings.py
+
+# settings.py
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    }
+}
